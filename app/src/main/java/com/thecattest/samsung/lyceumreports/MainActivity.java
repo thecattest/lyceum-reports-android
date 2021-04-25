869d973c455ee8597c402379a2e53a3e0cd22dd7
thecattest <vodopyanov999@gmail.com>
2021-04-25 22:48:55 +0300

    change status bar color

--- app/src/main/java/com/thecattest/samsung/lyceumreports/MainActivity.java
+++ app/src/main/java/com/thecattest/samsung/lyceumreports/MainActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
