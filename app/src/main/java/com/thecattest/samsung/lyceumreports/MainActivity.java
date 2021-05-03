134185c106f1511560f59dff6d7cd647948b0d8f
thecattest <vodopyanov999@gmail.com>
2021-05-03 14:01:14 +0300

    list view

--- app/src/main/java/com/thecattest/samsung/lyceumreports/MainActivity.java
+++ app/src/main/java/com/thecattest/samsung/lyceumreports/MainActivity.java
@@ -1,43 +1,13 @@
 package com.thecattest.samsung.lyceumreports;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.gson.Gson;
-import com.thecattest.samsung.lyceumreports.DataServices.Day;
-import com.thecattest.samsung.lyceumreports.DataServices.DayPost;
-import com.thecattest.samsung.lyceumreports.DataServices.DayService;
-import com.thecattest.samsung.lyceumreports.DataServices.Student;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private Button dayButton;
+    private ListView summaryListView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,15 +19,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void findViews() {
-        dayButton = findViewById(R.id.dayButton);
+        summaryListView = findViewById(R.id.summaryList);
     }
 
     private void setListeners() {
-        dayButton.setOnClickListener(this::dayButtonClickListener);
-    }
 
-    public void dayButtonClickListener(View v) {
-        Intent i = new Intent(MainActivity.this, DayActivity.class);
-        startActivity(i);
     }
 }
