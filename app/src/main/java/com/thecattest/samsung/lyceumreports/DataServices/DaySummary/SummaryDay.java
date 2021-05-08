b5546f1a82d74d6b80e36f62e0060220a292ae1c
thecattest <vodopyanov999@gmail.com>
2021-05-08 21:20:39 +0300

    day summary data classes

---
+++ app/src/main/java/com/thecattest/samsung/lyceumreports/DataServices/DaySummary/SummaryDay.java
@@ -0,0 +1,8 @@
+package com.thecattest.samsung.lyceumreports.DataServices.DaySummary;
+
+import java.util.ArrayList;
+
+class SummaryDay {
+    public String date;
+    public ArrayList<SummaryDayRow> groups;
+}
---
+++ app/src/main/java/com/thecattest/samsung/lyceumreports/DataServices/DaySummary/SummaryDayRow.java
@@ -0,0 +1,36 @@
+package com.thecattest.samsung.lyceumreports.DataServices.DaySummary;
+
+import java.util.ArrayList;
+
+public class SummaryDayRow {
+    public int id;
+    public String name;
+    public String status;
+    public ArrayList<String> students;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbsentStudentsString() {
+        if (status.equals(STATUS.OK)) {
+            if (students.size() != 0) {
+                StringBuilder absentStudentsString = new StringBuilder();
+                for (String absentStudent : students) {
+                    absentStudentsString.append(absentStudent);
+                    absentStudentsString.append(", ");
+                }
+                return absentStudentsString.substring(0, absentStudentsString.length() - 2);
+            } else {
+                return "Все в классе";
+            }
+        } else {
+            return "Нет данных";
+        }
+    }
+
+    public static class STATUS {
+        public static String OK = "ok";
+        public static String EMPTY = "empty";
+    }
+}
